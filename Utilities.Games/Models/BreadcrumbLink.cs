b01039e65cbaef39c0b8bfa7b333ef2ce472a789
Trais McAllister <tbm0115@gmail.com>
2021-11-29 00:37:55 -0800

    Tbm0115/lotr rise to war/commander icons (#6)
    * Added commander icons
    
    * Fixed image names, implemented images and added breadcrumbs

---
+++ Utilities.Games/Models/BreadcrumbLink.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Utilities.Games.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <see href="https://stackoverflow.com/a/64721882/4585104"/>
+    public class BreadcrumbLink
+    {
+        public int OrderIndex { get; set; }
+
+        public string Address { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+    public class BreadcrumbConfig {
+        public string Address { get; set; }
+
+        public string Title { get; set; }
+    }
+}
