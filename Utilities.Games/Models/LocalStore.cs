66708b724aa7c866bea72cf076a88cea31af79e2
Trais McAllister <tbm0115@gmail.com>
2021-11-30 02:15:53 -0800

    Tbm0115/lotr rise to war/indexed db (#11)
    * Initial implementation of IndexedDb across the site.
    
    * Added ability to add User Server Commanders

---
+++ Utilities.Games/Models/LocalStore.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.JSInterop;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Utilities.Games.Models
+{
+    public abstract class LocalStore<T>
+    {
+        /// <summary>
+        /// Unique key for the subsite this localstore is tied to.
+        /// </summary>
+        public abstract string SubsiteKey { get; }
+
+        /// <summary>
+        /// Optional override of the local store that maintains local edits.
+        /// </summary>
+        public abstract string STORE_NAME { get; }
+
+        /// <summary>
+        /// Optional override of the local store's prefix.
+        /// </summary>
+        public virtual string PREFIX => $"{SubsiteKey}_localStore";
+
+        private readonly HttpClient httpClient;
+        private readonly IJSRuntime js;
+
+        public LocalStore(HttpClient httpClient, IJSRuntime js)
+        {
+            this.httpClient = httpClient;
+            this.js = js;
+        }
+
+        public abstract object GetKey(T item);
+
+        public async Task<T> Get(object key)
+            => await GetAsync<T>(STORE_NAME, key);
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            IEnumerable<T> items = await GetAllAsync(STORE_NAME);
+            return items;
+        }
+        ValueTask<IEnumerable<T>> GetAllAsync(string storeName)
+            => js.InvokeAsync<IEnumerable<T>>($"{PREFIX}.getAll", storeName);
+
+        public virtual ValueTask SaveAsync(T item) => PutAsync(STORE_NAME, null, item);
+
+        ValueTask<U> GetAsync<U>(string storeName, object key)
+            => js.InvokeAsync<U>($"{PREFIX}.get", storeName, key);
+
+
+        ValueTask PutAsync<U>(string storeName, object key, U value)
+            => js.InvokeVoidAsync($"{PREFIX}.put", storeName, key, value);
+
+        ValueTask DeleteAsync(string storeName, object key)
+            => js.InvokeVoidAsync($"{PREFIX}.delete", storeName, key);
+    }
+}
---
+++ Utilities.Games/Pages/Subsites/LOTR_RiseToWar/Models/LocalStores/ServerStore.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Utilities.Games.Models;
+
+namespace Utilities.Games.Pages.Subsites.LOTR_RiseToWar.Models.LocalStores
+{
+    public static class LOTR_RiseToWar_LocalStore {
+        public const string SUBSITE_KEY = "LOTR_RiseToWar";
+    }
+    public class ServerStore : LocalStore<UserServer>
+    {
+        public override string SubsiteKey => LOTR_RiseToWar_LocalStore.SUBSITE_KEY;
+        public override string STORE_NAME => "userServers";
+
+        public ServerStore(HttpClient httpClient, IJSRuntime js) : base(httpClient, js) { }
+
+        public override object GetKey(UserServer item) => item.ServerNumber;
+
+        public override ValueTask SaveAsync(UserServer item)
+        {
+            item.LastUpdated = DateTime.UtcNow;
+            return base.SaveAsync(item);
+        }
+    }
+    public class CommanderStore : LocalStore<UserServerCommander>
+    {
+        public override string SubsiteKey => LOTR_RiseToWar_LocalStore.SUBSITE_KEY;
+        public override string STORE_NAME => "userCommanders";
+
+        public CommanderStore(HttpClient httpClient, IJSRuntime js) : base(httpClient, js) { }
+
+        public override object GetKey(UserServerCommander item) => item.Id;
+
+        public override ValueTask SaveAsync(UserServerCommander item)
+        {
+            item.Id = $"{item.ServerNumber}:{item.CommanderName}";
+            item.LastUpdated = DateTime.UtcNow;
+            return base.SaveAsync(item);
+        }
+    }
+    /// <summary>
+    /// Reference to a server that the user has joined.
+    /// </summary>
+    public class UserServer {
+        /// <summary>
+        /// Unique number for the server the user has joined.
+        /// </summary>
+        [Required]
+        public int ServerNumber { get; set; }
+
+        /// <summary>
+        /// Username of the user on the specified server.
+        /// </summary>
+        [Required]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Unique number for the user on the specified server.
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Name of the faction the user chose to side with.
+        /// </summary>
+        [Required]
+        public string Faction { get; set; }
+
+        /// <summary>
+        /// Unique ID of the Fellowship/Warband the user joined.
+        /// </summary>
+        [MaxLength(4)]
+        public string FellowshipId { get; set; }
+
+        /// <summary>
+        /// Name of the Fellowship/Warband the user joined.
+        /// </summary>
+        public string FellowshipName { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public class UserServerCommander {
+        public string Id { get; set; }
+
+        [Required]
+        public int ServerNumber { get; set; }
+
+        [Required]
+        public string CommanderName { get; set; }
+
+        public int CurrentLevel { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        // TODO: Maintain Skill Levels
+
+        // TODO: Maintain Equipment + Equipment Levels
+    }
+}
