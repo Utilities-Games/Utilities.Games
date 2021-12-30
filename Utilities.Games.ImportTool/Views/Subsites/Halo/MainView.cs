b5e56c0bb3179990a6fd7d394e192b47c811a504
Trais McAllister <tbm0115@gmail.com>
2021-12-30 10:19:17 -0800

    Added Halo subsite (#50)

---
+++ Utilities.Games.ImportTool/Views/Subsites/Halo/MainView.cs
@@ -0,0 +1,35 @@
+﻿using ConsoulLibrary.Views;
+using System;
+using System.Collections.Generic;
+using Utilities.Games.Models.Subsites.Halo;
+
+namespace Utilities.Games.ImportTool.Views.Subsites.Halo
+{
+    public class MainView : SubsiteView<MainView.Model>
+    {
+        public MainView() : base("Halo")
+        {
+        }
+
+        [DynamicViewOption(nameof(_saveDataModelWorkbook_message), nameof(_saveDataModelWorkbook_color))]
+        public override void SaveDataModelWorkbook()
+        {
+            base.SaveDataModelWorkbook();
+        }
+
+        protected override IEnumerable<Type> InitializeDatasetTypes()
+        {
+            return new List<Type>
+            {
+                typeof(Installment),
+            };
+        }
+
+        public class Model : ISubsiteViewModel
+        {
+            public string Title { get; set; }
+
+            public IEnumerable<Type> DatasetTypes { get; set; }
+        }
+    }
+}
