51256580aa27bcdc35932d347fee23be038ad389
tbm0115 <tbm0115@gmail.com>
2022-01-03 10:06:20 -0800

    Migrated to .net6 and implemented PageTitle component

--- Utilities.Games/Program.cs
+++ Utilities.Games/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.JSInterop;
@@ -17,7 +18,7 @@ namespace Utilities.Games
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<Utilities.Games.Models.Notifications.NotificationTriggers>();
